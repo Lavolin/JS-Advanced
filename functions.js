8b89a816d984781f57a42400654489cf66a4ff91
Lavolin <lavolin@gmail.com>
2023-01-15 17:33:10 +0200

    15.01.23

---
+++ functions.js
@@ -0,0 +1,33 @@
+function printStars(count){
+    console.log('*'.repeat(count));
+}
+
+printStars(10);
+
+//Function expression
+
+let countDown = function(number){
+    for (let i = number; i > 0; i--){
+        console.log(i);
+    }
+}
+
+countDown(10);
+
+//Arrow function
+
+let countUp = (max) => {
+    for (let i = 0; i <= max; i++){
+        console.log(i);
+    }
+}
+
+countUp(5)
+
+//Return value 
+
+function sum(a, b){
+    return a + b;
+}
+
+let sumArrow = (a, b) => a + b;
